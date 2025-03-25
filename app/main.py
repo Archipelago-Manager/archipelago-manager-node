334d14e5383f015903d46913af92727cd7136ffb
NaabZer <anton.gefvert@gmail.com>
2025-03-25 21:31:15 +0100

    Setup main FastAPI SQLModel functions

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from app.db import create_db_and_tables
+# from app.api.routers import users, games, hubs
+from app.models.servers import Server
+from app import models
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+# app.include_router(hubs.router)
